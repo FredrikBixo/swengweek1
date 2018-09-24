7914354254fc33cbf44076c7f55ee20209394e8d
Fredrik Bixo <fredrikbixo@Fredriks-MacBook-Pro-2.local>
2018-09-24 13:16:34 +0200

    Adding printing

--- LinkedList.java
+++ LinkedList.java
@@ -1,13 +1,17 @@
 //https://www.geeksforgeeks.org/linked-list-set-1-introduction/
 class LinkedList{
-    Node head;
+  Node head;
 
-    class Node {
-        int data;
-        Node next;
+  public void printList(){
+    Node n = head; while (n != null){ System.out.print(n.data+" "); n = n.next; }
+  }
 
-        Node(int d) {
-            data = d;
-        }
+  class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+      data = d;
     }
+  }
 }
